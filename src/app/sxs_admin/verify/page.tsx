caf2b91594132c71e5659609916f85787c631567
prayasjain8765@gmail.com <prayasjain8765@gmail.com>
2025-07-06 15:26:22 +0530

    Admin Login

--- src/app/sxs_admin/verify/page.tsx
+++ src/app/sxs_admin/verify/page.tsx
@@ -2,6 +2,7 @@ import OtpVerifyForm from "@/components/auth/OtpVerifyForm";
 
 
 export default function VerifyPage() {
+  //VErify OTP
   return (
     <main className="min-h-screen flex items-center justify-center bg-gradient-to-b from-[#050716] to-[#0F142A] px-4">
       <OtpVerifyForm />
